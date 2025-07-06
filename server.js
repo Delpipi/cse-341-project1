be14910408c4e78974a7fc3b8c314f5bd54822ba
Delpipi <alexandrepaulkouame@gmail.com>
2025-07-06 02:50:00 +0000

    updated w01 assignment addin default port 10000

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use('/', require('./routes'));
 /*****************
 ******* env  *****
 ******************/
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 
 /* *****************************************
