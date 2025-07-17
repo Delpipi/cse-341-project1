7e8b599b3b22710bc08e821d90e948ba2832a70e
Delpipi <alexandrepaulkouame@gmail.com>
2025-07-17 12:43:33 +0000

    completed W03 Team Activity

--- controllers/contact-controller.js
+++ controllers/contact-controller.js
@@ -1,80 +1,59 @@
 const { getContactById, getAllContact, createContact, updateContact, deleteContact} = require('../models/contact-model');
-
+const appError = require('../utilities/app-error');
+const httpStatusCodes = require('../utilities/http-status-code');
 const contactController = {};
 
 contactController.getContactById = async (req, res) => {
-    try {
-        const id = req.params.id;
-        const result = await getContactById(id);
-        res.setHeader('Content-type', 'application/json');
-        if (result) {
-            res.status(200).json(result);
-        } else {
-            res.status(404).json({message: "Contact not found"});
-        }
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Server unexpected error');
+    const id = req.params.id;
+    const result = await getContactById(id);
+    res.setHeader('Content-Type', 'application/json');
+    if (result) {
+        res.status(httpStatusCodes.OK).json(result);
+    } else {
+        throw new appError(httpStatusCodes.NOT_FOUND, "Contact not found");
     }
 }
 
 contactController.getAllContact = async (req, res) => {
-    try {
-        const result = await getAllContact();
-        res.setHeader('Content-type', 'application/json');
-        res.status(200).json(result);
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Server unexpected error');
-    }
+    const result = await getAllContact();
+    res.setHeader('Content-type', 'application/json');
+    res.status(httpStatusCodes.OK).json(result);
 }
 
-contactController.createContact = async (req, res, next) => {
+contactController.createContact = async (req, res) => {
     const { firstName, lastName, email, favoriteColor, birthday } = req.body;
-    try {
-        const result = await createContact(firstName, lastName, email, favoriteColor, birthday);
-        res.setHeader('Content-Type', 'application/json');
-        if (result.acknowledged) {
-            res.status(201).json({message: `the Contact '${firstName} ${lastName}' successfully created`});
-        } else {
-            res.status(400).json({message: `Create contact '${firstName} ${lastName}' failed`});
-        }
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Server unexpected error');
+    const result = await createContact(firstName, lastName, email, favoriteColor, birthday);
+
+    res.setHeader('Content-Type', 'application/json');
+    if (result.acknowledged) {
+        res.status(httpStatusCodes.CREATED).json({message: `the Contact '${firstName} ${lastName}' successfully created`});
+    } else {
+        throw new appError(httpStatusCodes.BAD_REQUEST, `Create contact '${firstName} ${lastName}' failed`);
     }
 }
 
-contactController.updateContact = async (req, res, next) => {
+contactController.updateContact = async (req, res) => {
     const id = req.params.id;
     const {firstName, lastName, email, favoriteColor, birthday } = req.body;
-    try {
-        const result = await updateContact(id, firstName, lastName, email, favoriteColor, birthday);
-        res.setHeader('Content-Type', 'application/json');
-        if (result.modifiedCount > 0) {
-            res.status(200).json({message: `The contact '${firstName} ${lastName}' successfully updated`});
-        } else {
-            res.status(400).json({message: `Update contact '${firstName} ${lastName}' failed`});
-        }
-    } catch (error) {
-        console.error(err);
-        res.status(500).send('Server unexpected error');
+    
+    const result = await updateContact(id, firstName, lastName, email, favoriteColor, birthday);
+    res.setHeader('Content-Type', 'application/json');
+    if (result.modifiedCount > 0) {
+        res.status(httpStatusCodes.OK).json({message: `The contact '${firstName} ${lastName}' successfully updated`});
+    } else {
+        throw new appError(httpStatusCodes.BAD_REQUEST, `Update contact '${firstName} ${lastName}' failed`);
     }
 }
 
-contactController.deleteContact = async (req, res, next) => {
+contactController.deleteContact = async (req, res) => {
     const id = req.params.id;
-    try {
-        const result = await deleteContact(id);
-        res.setHeader('Content-Type', 'application/json');
-        if (result.deletedCount > 0) {
-            res.status(200).json({ message: 'The contact successfully deleted'});
-        } else {
-            res.status(400).json({ message: 'Delete contact failed' });
-        }
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Server unexpected error');
+   
+    const result = await deleteContact(id);
+    res.setHeader('Content-Type', 'application/json');
+    if (result.deletedCount > 0) {
+        res.status(httpStatusCodes.NO_CONTENT).send();
+    } else {
+        throw new appError(httpStatusCodes.BAD_REQUEST, 'Delete contact failed');
     }
 }
 
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,12 +1,35 @@
 const router = require('express').Router();
 
+const utilities = require('../utilities');
+const validate = require('../utilities/contact-validation');
 const contactController = require('../controllers/contact-controller');
 
 //GET routes
-router.get('/', contactController.getAllContact);
-router.get('/:id', contactController.getContactById);
-router.post('/', contactController.createContact);
-router.put('/:id', contactController.updateContact);
-router.delete('/:id', contactController.deleteContact);
+router.get('/', utilities.handleErrors(contactController.getAllContact));
+
+router.get('/:id',
+    validate.contactIdRules(),
+    validate.checkRules,
+    utilities.handleErrors(contactController.getContactById)
+);
+
+router.post('/',
+    validate.contactRules(),
+    validate.checkRules,
+    utilities.handleErrors(contactController.createContact)
+);
+
+router.put('/:id',
+    validate.contactIdRules(),
+    validate.contactRules(),
+    validate.checkRules,
+    utilities.handleErrors(contactController.updateContact)
+);
+
+router.delete('/:id',
+    validate.contactIdRules(),
+    validate.checkRules,
+    utilities.handleErrors(contactController.deleteContact)
+);
 
 module.exports = router;
